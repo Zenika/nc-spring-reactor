0de8f664c04ad9c6eb359a1b96eb59fd953c24b8
Mick <mickael.boixiere@zenika.com>
2019-06-25 17:21:22 +0200

    step4-correction

--- data/src/main/java/com/zenika/nc/data/web/DataController.java
+++ data/src/main/java/com/zenika/nc/data/web/DataController.java
@@ -1,28 +1,41 @@
 package com.zenika.nc.data.web;
 
 import com.zenika.nc.data.model.Temperature;
+import com.zenika.nc.data.repository.TemperatureRepository;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RestController
 public class DataController {
 
+    private final TemperatureRepository temperatureDataRepository;
     private final DataWebClient webClient;
 
-    public DataController(DataWebClient webClient) {
+    public DataController(TemperatureRepository temperatureDataRepository, DataWebClient webClient) {
+        this.temperatureDataRepository = temperatureDataRepository;
         this.webClient = webClient;
     }
 
     @GetMapping(path = "all", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Temperature> getAll() {
-        return Flux.empty();
+        return temperatureDataRepository.findAll();
     }
 
     @GetMapping(path = "limit/{value}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<Temperature> getWithLimit(@PathVariable(name = "value") Integer count) {
-        return Flux.empty();
+        final AtomicInteger atomicInteger = new AtomicInteger(0);
+        return temperatureDataRepository.findAll()
+                .handle((temperature, sink) -> {
+                    if (atomicInteger.getAndIncrement() == count) {
+                        sink.complete();
+                        return;
+                    }
+                    sink.next(temperature);
+                });
     }
 }
