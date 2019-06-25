5b6e16d6a5a16a0f2914c3753915d8d2367904c2
Mick <mickael.boixiere@zenika.com>
2019-06-25 17:31:45 +0200

    step5-correction

--- data/src/main/java/com/zenika/nc/data/utils/MongoListener.java
+++ data/src/main/java/com/zenika/nc/data/utils/MongoListener.java
@@ -5,14 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.FluxSink;
 
 @Component
 @Slf4j
 public class MongoListener extends AbstractMongoEventListener<Temperature> {
 
+    private final FluxSink<Temperature> senderIncoming;
+
+    public MongoListener(FluxSink<Temperature> senderIncoming) {
+        this.senderIncoming = senderIncoming;
+    }
+
     @Override
     public void onAfterSave(AfterSaveEvent<Temperature> event) {
         log.info("onAfterSave event: " + event.toString());
+        senderIncoming.next(event.getSource());
         super.onAfterSave(event);
     }
 }
--- data/src/main/java/com/zenika/nc/data/web/DataController.java
+++ data/src/main/java/com/zenika/nc/data/web/DataController.java
@@ -2,12 +2,14 @@ package com.zenika.nc.data.web;
 
 import com.zenika.nc.data.model.Temperature;
 import com.zenika.nc.data.repository.TemperatureRepository;
+import com.zenika.nc.data.utils.JacksonConverter;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.DirectProcessor;
 
 import java.time.LocalTime;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -16,11 +18,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class DataController {
 
     private final TemperatureRepository temperatureDataRepository;
-    private final DataWebClient webClient;
+    private final DirectProcessor<Temperature> senderProcessor;
 
-    public DataController(TemperatureRepository temperatureDataRepository, DataWebClient webClient) {
+    public DataController(TemperatureRepository temperatureDataRepository, DirectProcessor<Temperature> senderProcessor) {
         this.temperatureDataRepository = temperatureDataRepository;
-        this.webClient = webClient;
+        this.senderProcessor = senderProcessor;
     }
 
     @GetMapping(path = "all", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
@@ -43,7 +45,12 @@ public class DataController {
 
     @GetMapping(path = "event", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ServerSentEvent<String>> getTemperatureEvent() {
-        return Flux.empty();
+        return senderProcessor
+                .log()
+                // map object to json payload
+                .map(JacksonConverter::serialize)
+                // map json payload to server sent event object
+                .map(this::toServerSentEvent);
     }
 
     private ServerSentEvent<String> toServerSentEvent(String payload) {
